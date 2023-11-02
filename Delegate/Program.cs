1c68bb02623d5f63ba25a450d587fc5219def89b
Артем Кунченко <kunchenko911@ukr.net>
2023-11-02 23:54:43 +0200

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Delegate
+{
+    internal class Program
+    {
+        public delegate double MathOperation(double a, double b);
+        public static double Add(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a * b;
+        public static double Divide(double a, double b)=> (b == 0) ? 0 : a / b;
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
