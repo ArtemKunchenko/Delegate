afbc5a840ac3f1b15eec639c7dacff7817d2b436
Артем Кунченко <kunchenko911@ukr.net>
2023-11-03 22:09:54 +0200

    Added method operation()

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static Delegate.Program;
 
 
 namespace Delegate
@@ -12,6 +13,57 @@ namespace Delegate
         public static double Divide(double a, double b)=> (b == 0) ? 0 : a / b;
         static void Main(string[] args)
         {
+            operation();
+            Console.ReadKey();
+        }
+         static void operation ()
+        {
+            int choice = 0;
+            char s=' ';
+            MathOperation mathOperation=null;
+            Console.Write("Input first operand: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Input second operand: ");
+            double b = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Choose operation:");
+                Console.WriteLine("Addition - 1");
+                Console.WriteLine("Subtraction - 2");
+                Console.WriteLine("Multiplication - 3");
+                Console.WriteLine("Devivion - 4");
+                choice=  int.Parse(Console.ReadLine());
+                if (choice >= 1 && choice <= 4) break;
+                Console.WriteLine("Ivalid value");
+
+            } while (choice<1||choice>4);
+            switch (choice)
+            {
+                case 1:
+                    mathOperation = Add;
+                    s = '+';
+                    break;
+                case 2:
+                    mathOperation = Subtract;
+                    s = '-';
+                    break;
+                case 3:
+                    mathOperation = Multiply;
+                    s = '*';
+                    break;
+                case 4:
+                    mathOperation = Divide;
+                    s = '/';
+                    break;
+                default:
+                    
+                    break;
+            }
+            Console.Clear();
+            double result = mathOperation(a, b);
+            Console.WriteLine($"{a}{s}{b}={result}");
         }
 
     }
